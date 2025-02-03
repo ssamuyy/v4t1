131282a834f50369839d829408b753dd6c328fee
ssamuyy <samu.yrttiaho@student.lut.fi>
2025-02-03 20:34:53 +0200

    Update App.java

--- src/main/java/main/App.java
+++ src/main/java/main/App.java
@@ -29,7 +29,7 @@ public class App {
                     cave.addMonster(new Monster(type, health));
                     break;
                 case 2:
-                    System.out.println("Luolan hirviöt:");
+                    System.out.print("Luolan hirviöt:");
                     cave.listMonsters();
                     break;
                 case 3:
